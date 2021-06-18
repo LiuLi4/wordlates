6f729968a31b88159e390e058b7859e8e7b395c0
LiuLi4 <18245284519@163.com>
2021-06-18 11:39:24 +0800

    前后端结合

--- src/test/java/com/bjfu/wordlates/utils/FILEUtilTest.java
+++ src/test/java/com/bjfu/wordlates/utils/FILEUtilTest.java
@@ -3,14 +3,12 @@ package com.bjfu.wordlates.utils;
 import com.bjfu.wordlates.constant.Constants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
